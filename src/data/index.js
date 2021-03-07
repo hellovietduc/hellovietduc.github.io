9a5a3a27948721dd438eb2dfe8e8f3f80355f72d
Duc Nguyen <vietduc01100001@gmail.com>
2021-03-07 13:10:36 +0700

    point project links to github

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 
+const GITHUB_URL = 'https://github.com/hellovietduc';
 const NUMBER_OF_404_IMG = 23;
 
 const getRandom404Image = () => {
@@ -13,36 +14,36 @@ const projects = [
     name: 'The Playground',
     description: 'A code playground website using Docker power 🔥 to run multiple programming languages',
     preview: require('./preview/theplayground.png'),
-    source: 'https://github.com/hellovietduc/theplayground',
-    website: 'https://playground.duc-nguyen.xyz'
+    source: `${GITHUB_URL}/theplayground`,
+    website: `${GITHUB_URL}/theplayground`
   },
   {
     name: 'Envidict',
     description: 'A lightning-fast ⚡ English-Vietnamese dictionary website',
     preview: require('./preview/envidict.png'),
-    source: 'https://github.com/hellovietduc/envidict',
-    website: 'https://envidict.duc-nguyen.xyz'
+    source: `${GITHUB_URL}/envidict`,
+    website: `${GITHUB_URL}/envidict`
   },
   {
     name: 'web-crawler',
     description: 'A program that crawls text data from a website at any depth level',
     preview: getRandom404Image(),
-    source: 'https://github.com/hellovietduc/web-crawler',
-    website: 'https://github.com/hellovietduc/web-crawler/releases'
+    source: `${GITHUB_URL}/web-crawler`,
+    website: `${GITHUB_URL}/web-crawler`
   },
   {
     name: 'gomoku-cli',
     description: 'Play Gomoku in command line',
     preview: getRandom404Image(),
-    source: 'https://github.com/hellovietduc/gomoku-cli',
-    website: 'https://github.com/hellovietduc/gomoku-cli/releases'
+    source: `${GITHUB_URL}/gomoku-cli`,
+    website: `${GITHUB_URL}/gomoku-cli`
   },
   {
     name: 'video-converter-web',
     description: 'A video converter website',
     preview: getRandom404Image(),
-    source: 'https://github.com/hellovietduc/video-converter-web',
-    website: 'https://github.com/hellovietduc/video-converter-web/releases'
+    source: `${GITHUB_URL}/video-converter-web`,
+    website: `${GITHUB_URL}/video-converter-web`
   }
 ];
 
