664de9f9fe6dfbc62ab95271095ea6dbdeef7e72
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-25 21:41:28 +0700

    add header component

---
+++ src/layouts/Header.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import metadata from '../metadata.json';
+
+withDefaults(
+  defineProps<{
+    extended?: boolean;
+  }>(),
+  {
+    extended: false,
+  }
+);
+</script>
+
+<template>
+  <header>
+    <div>
+      <img src="logo.png" :alt="`${metadata.blogName}'s logo`" />
+    </div>
+    <template v-if="extended">
+      <h1>
+        {{ metadata.blogName }}
+      </h1>
+      <h2>
+        {{ metadata.blogDescription }}
+      </h2>
+    </template>
+  </header>
+</template>
