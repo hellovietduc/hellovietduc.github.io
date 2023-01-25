6ff91363982d24fbb72ce22d629c48dd171756cc
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-25 23:07:05 +0700

    add post card component

---
+++ src/components/PostCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import Link from './Link.vue';
+
+const props = defineProps<{
+  title: string;
+  description: string;
+  publishedDate: string | number | Date;
+  url: string;
+}>();
+
+const formattedDate = computed(() => {
+  const date = new Date(props.publishedDate);
+  return date.toLocaleDateString();
+});
+</script>
+
+<template>
+  <Link :url="url">
+    <article>
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+      <time :datetime="publishedDate.toString()">{{ formattedDate }}</time>
+    </article>
+  </Link>
+</template>
---
+++ src/layouts/PostList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import PostCard from '../components/PostCard.vue';
+
+const posts = [{
+  title: 'Hello World',
+  description: 'This is a description',
+  createdAt: '2023-01-01',
+  url: '/hello-world',
+}]
+</script>
+
+<template>
+  <div>
+    <PostCard v-for="post in posts" v-bind="{ ...post }" />
+  </div>
+</template>
