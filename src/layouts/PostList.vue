7e0a7b4d4e350f02aa43d7e45b5043017c7bf8ff
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-26 00:36:51 +0700

    render post list

--- src/layouts/PostList.vue
+++ src/layouts/PostList.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+import { getCollection } from 'astro:content';
 import PostCard from '../components/PostCard.vue';
+import { isPostPublished } from '../helper/blog';
 
-const posts = [{
-  title: 'Hello World',
-  description: 'This is a description',
-  createdAt: '2023-01-01',
-  url: '/hello-world',
-}]
+const allPosts = await getCollection('blog');
+const publishedPosts = allPosts
+  .filter((post) => isPostPublished(post.data))
+  .map((post) => ({ ...post.data, url: `/posts/${post.slug}` }));
 </script>
 
 <template>
   <div>
-    <PostCard v-for="post in posts" v-bind="{ ...post }" />
+    <PostCard
+      v-for="post in publishedPosts"
+      :url="post.url"
+      :title="post.title"
+      :description="post.description"
+      :tags="post.tags"
+      :published-date="post.publishedDate"
+    />
   </div>
 </template>
