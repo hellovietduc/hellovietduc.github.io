7f6923ee6456c11c6aab5c6277fd07d97246c7f8
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-26 15:34:49 +0700

    sort posts by published date

--- src/helper/blog.ts
+++ src/helper/blog.ts
@@ -2,7 +2,7 @@ import { getCollection } from 'astro:content';
 
 export const getPublishedBlogEntries = async () => {
   const entries = await getCollection('blog');
-  return entries.filter(
-    ({ data }) => !data.draft && data.publishedDate != null
-  );
+  return entries
+    .filter(({ data }) => !data.draft && data.publishedDate != null)
+    .sort((a, b) => b.data.publishedDate.getTime() - a.data.publishedDate.getTime());
 };
