6bf13f7ea8d8886992b742ee56dc6ed2e9efe353
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-26 15:34:38 +0700

    blog post elements spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontSize: {
+        '1.5xl': ['1.375rem', '2rem'],
+        '2.5xl': ['1.675rem', '2.25rem'],
+        '3.5xl': ['2.125rem', '2.5rem'],
+      },
+    },
+  },
+  plugins: [],
+};
