ac62ed7a29ac22d7b9267df586cee4897ce87aea
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-26 21:09:42 +0700

    generalize post list component

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { CollectionEntry } from 'astro:content';
+import PostCard from './PostCard.vue';
+
+type Post = CollectionEntry<'blog'>['data'] & { url: string };
+
+defineProps<{
+  posts: Post[];
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-8">
+    <PostCard
+      v-for="post in posts"
+      :url="post.url"
+      :title="post.title"
+      :description="post.description"
+      :tags="post.tags"
+      :published-date="post.publishedDate"
+    />
+  </div>
+</template>
