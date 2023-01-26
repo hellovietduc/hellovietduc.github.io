dee012fbc79b20e2a43a1c7d81d93ea759f32f55
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-26 12:41:01 +0700

    convert footer to vue

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import UsefulLinks from '../components/UsefulLinks.vue';
+</script>
+
+<template>
+  <footer>
+    <slot name="top" />
+    <UsefulLinks />
+  </footer>
+</template>
