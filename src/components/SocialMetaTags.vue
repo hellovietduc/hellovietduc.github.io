11e0b562c409dfad9c6291f29e5778256985c4f1
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-26 12:33:42 +0700

    convert social meta tags to vue

---
+++ src/components/SocialMetaTags.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  contentType: string;
+  url: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+}>();
+</script>
+
+<template>
+  <meta property="og:type" :content="contentType" />
+  <meta property="og:url" :content="url" />
+  <meta property="og:title" :content="title" />
+  <meta property="og:description" :content="description" />
+  <meta property="og:image" :content="imageUrl" />
+</template>
