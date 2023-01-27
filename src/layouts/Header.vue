61557997a41280e242fca13fb2adcb6a05a056d3
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-01-27 14:58:34 +0700

    optimize for phone + text justify

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -14,7 +14,7 @@ withDefaults(
 
 <template>
   <header
-    :class="['flex flex-col items-center gap-4', extended ? 'mb-12' : 'mb-8']"
+    :class="['flex flex-col items-center gap-4', extended ? 'mb-12' : 'mb-8', 'text-center']"
   >
     <div>
       <Link url="/" :aria-label="`${metadata.blogName}'s logo`">
