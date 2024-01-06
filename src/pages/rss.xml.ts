510916a82cb0abe532414b3f7d7ce1cef924d127
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2024-01-07 03:19:11 +0800

    fix RSS handler method

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,21 @@
-import rss from '@astrojs/rss';
-import { getPublishedBlogEntries } from '@helpers/blog';
-import metadata from '@metadata.json';
-import type { APIRoute } from 'astro';
+import rss from '@astrojs/rss'
+import { getPublishedBlogEntries } from '@helpers/blog'
+import metadata from '@metadata.json'
+import type { APIRoute } from 'astro'
 
-export const get: APIRoute = async (context) => {
-  const blogEntries = await getPublishedBlogEntries();
+export const GET: APIRoute = async (context) => {
+  const blogEntries = await getPublishedBlogEntries()
   const rssItems = blogEntries.map((entry) => ({
     title: entry.data.title,
     description: entry.data.description,
     pubDate: entry.data.publishedDate,
     link: `/posts/${entry.slug}`,
-  }));
+  }))
   return rss({
     title: metadata.blogName,
     description: metadata.blogDescription,
     site: context.site?.toString() ?? '',
     items: rssItems,
     customData: '<language>en-us</language>',
-  });
-};
+  })
+}
