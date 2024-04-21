2a0d55ca61e7cbd11538a43bfea918537f7d46e9
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2024-04-21 13:00:38 +0800

    add header link

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import vue from '@astrojs/vue'
 import compress from 'astro-compress'
 import robotsTxt from 'astro-robots-txt'
 import { defineConfig, squooshImageService } from 'astro/config'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import { h } from 'hastscript'
+
+const headerLink = h('span', '#')
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,5 +27,17 @@ export default defineConfig({
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'github-dark-dimmed',
     },
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          headingProperties: { class: 'heading' },
+          properties: { class: 'heading-link' },
+          behavior: 'prepend',
+          content: [headerLink],
+        },
+      ],
+    ],
   },
 })
