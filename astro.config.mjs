173ad4888d6943a68fe8b7818aff892e6e2416fe
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2024-04-21 15:03:51 +0800

    change code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'github-dark-dimmed',
+      theme: 'rose-pine-dawn',
     },
     rehypePlugins: [
       rehypeSlug,
